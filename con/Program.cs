22362923fc5ae8a8d1878f0f37c0f8668b982e62
user <sean.saap@studytafensw.edu.au>
2023-11-27 18:01:35 +1100

    initial

---
+++ con/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace con;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string pathU = "../../../input/in.txt";
+        string pathS = "../../../save/test.csv";
+
+        string dream;
+        StreamReader scan = new StreamReader(pathU);
+        StreamWriter print = new StreamWriter(pathS);
+
+        dream = scan.ReadLine();
+        while (dream != null)
+        {
+
+            print.WriteLine(dream);
+
+
+
+            dream = scan.ReadLine();
+        }
+
+        scan.Close();
+        print.Close();
+
+            }
+}
+
